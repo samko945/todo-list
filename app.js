ba121250132ce84961889a717901f595be7db3f4
Samuel <samuel@samkos-mbp.local>
2021-05-05 20:24:43 +0100

    added 2nd page (/work) using same template

--- app.js
+++ app.js
@@ -4,27 +4,44 @@ const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 
-const items = [];
+app.use(express.static("public"));
+
+const items = ["Buy food", "Cook food", "Eat food"];
+const workItems = [];
 
 app.get("/", function (req, res) {
-    var today = new Date();
-    var options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long",
-    };
-    var day = today.toLocaleDateString("en-US", options);
+	var today = new Date();
+	var options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	};
+	var date = today.toLocaleDateString("en-US", options);
 
-    res.render("list", { dayOfWeek: day, listItems: items });
+	res.render("list", { listTitle: date, listItems: items });
 });
 
 app.post("/", function (req, res) {
-    const item = req.body.newItem;
-    console.log(item);
-    items.push(item);
-    res.redirect("/");
+	const item = req.body.newItem;
+	if (req.body.list === "Work") {
+		workItems.push(item);
+		res.redirect("/work");
+	} else {
+		items.push(item);
+		res.redirect("/");
+	}
 });
 
+app.get("/work", function (req, res) {
+	res.render("list", { listTitle: "Work", listItems: workItems });
+});
+
+// app.post("/work", function (req, res) {
+// 	const item = req.body.newItem;
+// 	workItems.push(item);
+// 	res.redirect("/work");
+// });
+
 app.listen(5000, function () {
-    console.log("Server started on port 5000.");
+	console.log("Server started on port 5000.");
 });
